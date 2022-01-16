2136be05a2470cb703191134e99a1d0c2ebc439c
uansari <uansari@polariswireless.com>
2022-01-16 10:51:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// import http from "http";
+
+// const server = http.createServer((req, res) => {
+//   console.log("req", req);
+//   console.log("res", res);
+//   res.end("Hello World");
+//   res.writeHead(200, { "Content-Type": "application/json" });
+// });
+
+// server.listen(3000);
+
+//--------------------------------------------------
+
+import express from "express";
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Started express");
+});
+
+app.get("/api/characters", (req, res) =>
+  res.send({
+    results: [
+      {
+        name: "John",
+      },
+    ],
+    sucess: "true",
+  })
+);
+
+app.listen(4002);
